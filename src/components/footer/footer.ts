523b2d19c270dfca74fbfedbecf23d4f737ebadb
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-04-14 14:02:00 +0200

    fix css img

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,9 +1,26 @@
 import './footer.scss';
 import footerTemplate from './footer.html?raw';
 
+import shodoWhiteImg from '../../assets/img/logo-shodo.svg';
+import shodoBlackImg from '../../assets/img/logo-shodo-black.svg';
+
 export class CustomFooter extends HTMLElement {
   async connectedCallback() {
     this.innerHTML = footerTemplate;
+
+    const images = [
+      { id: 'img-shodo-white', src: shodoWhiteImg },
+      { id: 'img-shodo-black', src: shodoBlackImg },
+    ];
+
+    images.forEach((imgData) => {
+      const imgElement = document.getElementById(
+        imgData.id,
+      ) as HTMLImageElement;
+      if (imgElement) {
+        imgElement.src = imgData.src;
+      }
+    });
   }
 }
 
